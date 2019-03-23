54c0917fb782e1afea076a90963174f78dc833ae
meizhixing <meizhixing@ynao.ac.cn>
2019-03-23 16:25:16 +0000

    add mongodb, bodyparser, express-validator

--- routes/users.js
+++ routes/users.js
@@ -27,10 +27,10 @@ router.get('/register',function(req, res, next) {
 router.post('/register',
   [check('name').isLength({min: 1}).withMessage('Name is required'),
   check('username').isLength({min: 1}).withMessage('Username is required')],
-  function(req, res, next) {
+  function(req, res) {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    res.render('register', {errors: errors.array()});
+    return res.status(422).json({ errors: errors.array() });
   } else {
     res.send("register ...");
   }
