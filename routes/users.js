43e2b4f860980b46813bab6a1c347a1a69514544
meizhixing <meizhixing@ynao.ac.cn>
2019-03-23 23:38:10 +0800

    add mongodb, bodyparser, express-validator

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const {check, validationResult} = require('express-validator/check');
 
 router.get('/',function(req, res, next) {
   res.send("users homepage");
@@ -10,11 +11,29 @@ router.get('/login',function(req, res, next) {
   res.render('login', {title: 'Login'});
 });
 
+router.post('/login',function(req, res, next) {
+  res.send("username: " + req.body.username + " password: " + req.body.password);
+});
+
 router.get('/admin',function(req, res, next) {
   res.send("admin page");
 });
 
 router.get('/register',function(req, res, next) {
-  res.send("users register");
+//  res.send("users register");
+  res.render('register', {title: 'Register'});
 });
+
+router.post('/register',
+  [check('name').isLength({min: 1}).withMessage('Name is required'),
+  check('username').isLength({min: 1}).withMessage('Username is required')],
+  function(req, res, next) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.render('register', {errors: errors.array()});
+  } else {
+    res.send("register ...");
+  }
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,26 @@
-var express = require("express");
-var path = require("path");
+'use strict';
+const express = require("express");
+const path = require("path");
+const mongoose = require('mongoose');
 
 var usersRouter = require('./routes/users');
 var indexRouter = require('./routes/index');
 
+mongoose.connect('mongodb://mongodb/virtualhelper', {useNewUrlParser: true});
+let db = mongoose.connection;
+db.on('error',function(err) {
+  console.log(err);
+});
+db.once('open',()=>{console.log('connect to mongodb successfully')});
+
 const app = express();
+var bodyparser = require('body-parser');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname,'public')));
 app.use('/users',usersRouter);
 app.use('/',indexRouter);
